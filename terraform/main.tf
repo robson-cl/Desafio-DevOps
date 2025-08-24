0c119ade244b03b18f6f4e7c6241e037041d9504
robson-cl <robsonc.leite94@gmail.com>
2025-08-24 11:07:34 -0300

    adjusting iam permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,6 +115,11 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_task_ssm_attach" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+}
+
 # 8. ECS Task Definition
 resource "aws_ecs_task_definition" "task" {
   family                   = var.app_name
