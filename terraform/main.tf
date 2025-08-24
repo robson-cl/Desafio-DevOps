54666cdfdd0aa73c307edaefbc05e720480d244d
robson-cl <robsonc.leite94@gmail.com>
2025-08-24 13:11:00 -0300

    add logs nginx ecs

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,6 +149,7 @@ resource "aws_ecs_task_definition" "nginx_task" {
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([
     {
@@ -171,6 +172,28 @@ resource "aws_ecs_task_definition" "nginx_task" {
       }
     }
   ])
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecsTaskRole-nginx-proxy"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_role_ssm" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
 
 # 9. ECS Fargate Service
