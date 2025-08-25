43dcb1166c277ce5e26d1244bce14ca2aabb4b5b
robson-cl <robsonc.leite94@gmail.com>
2025-08-25 18:42:46 -0300

    fix nginx config

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "container_port" {
 
 variable "container_port_nginx" {
   description = "Porta do nginx"
-  default     = 443
+  default     = 80
 }
 
 variable "desired_count" {
@@ -29,7 +29,7 @@ variable "desired_count" {
 
 variable "alb_port" {
   description = "Porta do ALB"
-  default     = 443
+  default     = 80
 }
 
 variable "alb_health_check_path" {
