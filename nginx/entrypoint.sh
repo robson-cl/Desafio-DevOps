82a2308261ec00ea27dde757fdfe7238ba294ef6
robson-cl <robsonc.leite94@gmail.com>
2025-08-24 21:49:19 -0300

    adjusting certs

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -6,11 +6,12 @@ mkdir -p /etc/nginx/certs
 
 # Decodificar os certificados Base64
 base64 -d /etc/nginx/certs/scr  > /etc/nginx/certs/server.crt
-base64 -d /etc/nginx/certs/scs  > /etc/nginx/certs/server.key
+base64 -d /etc/nginx/certs/ske  > /etc/nginx/certs/server.key
+base64 -d /etc/nginx/certs/scs  > /etc/nginx/certs/server.cs
 base64 -d /etc/nginx/certs/cc   > /etc/nginx/certs/ca.crt
 
 # Opcional: remover arquivos Base64 decodificados
-rm /etc/nginx/certs/cc /etc/nginx/certs/scr /etc/nginx/certs/scs
+rm /etc/nginx/certs/cc /etc/nginx/certs/scr /etc/nginx/certs/scs /etc/nginx/certs/ske
 
 # Rodar Nginx em foreground
 exec nginx -g "daemon off;"
