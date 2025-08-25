7e4fcbb22c99cb16bcef0764b0885eaa0065a4c4
robson-cl <robsonc.leite94@gmail.com>
2025-08-25 10:16:23 -0300

    fix nginx config

--- terraform/main.tf
+++ terraform/main.tf
@@ -236,7 +236,7 @@ resource "aws_ecs_service" "service" {
   network_configuration {
     subnets         = [aws_subnet.public_a.id, aws_subnet.public_b.id]
     security_groups = [aws_security_group.ecs_sg.id]
-    assign_public_ip = true
+    assign_public_ip = false
   }
 
   service_registries {
@@ -306,7 +306,7 @@ resource "aws_ecs_service" "nginx_proxy" {
   network_configuration {
     subnets         = [aws_subnet.public_a.id, aws_subnet.public_b.id]
     security_groups = [aws_security_group.ecs_sg.id]
-    assign_public_ip = true
+    assign_public_ip = false
   }
 
   load_balancer {
