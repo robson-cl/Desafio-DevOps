01da2799803b89787ff091128692800ac3b2c1a0
robson-cl <robsonc.leite94@gmail.com>
2025-08-26 12:04:53 -0300

    fix terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -23,8 +23,9 @@ output "alb_arn" {
 }
 
 output "alb_target_group_arn" {
-  value = aws_lb_target_group.nginx_tg.arn
+  value = aws_lb_target_group.app_tg.arn
 }
+
 variable "nginx_desired_count" {
   description = "Número de tasks do Nginx"
   default     = 1
